c451657c689f8703aa9ecfe2123ea3b212180352
CRivas <christianrivasp@gmail.com>
2021-03-15 20:10:35 +0100

    7

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -13,8 +13,6 @@ import java.sql.SQLException;
 import models.db;
 
 
-
-
 public class mainApp {
 
 	/**
@@ -25,33 +23,32 @@ public class mainApp {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		db.connectDB("root", "");
-		db.createDB("PiezasDB");
-		db.createTable("PiezasDB", "CREATE TABLE Cientificos (Codigo INT NOT NULL, Nombre VARCHAR(100), PRIMARY KEY (Codigo));");
-		db.createTable("PiezasDB", "CREATE TABLE Proyecto (ID CHAR(4) NOT NULL, Nombre VARCHAR(100), PRIMARY KEY (ID));");
-		db.createTable("PiezasDB", "CREATE TABLE Suministra (CodigoPieza INT NOT NULL, idProveedor CHAR(4) NOT NULL, Precio INT,  PRIMARY KEY(CodigoPieza, idProveedor), FOREIGN KEY (CodigoPieza) REFERENCES Piezas(Codigo), FOREIGN KEY (idProveedor) REFERENCES Proveedores(id));");
-		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"1\", \"1\");");
-		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"2\", \"2\");");
-		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"3\", \"3\");");
-		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"4\", \"4\");");
-		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"5\", \"5\");");
-		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"1\", \"1\");");
-		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"2\", \"2\");");
-		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"3\", \"3\");");
-		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"4\", \"4\");");
-		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"5\", \"5\");");
-		db.insertData("PiezasDB", "INSERT INTO Suministra (CodigoPieza, idProveedor) VALUES (\"1\", \"1\");");
-		db.insertData("PiezasDB", "INSERT INTO Suministra (CodigoPieza, idProveedor) VALUES (\"2\", \"2\");");
-		db.insertData("PiezasDB", "INSERT INTO Suministra (CodigoPieza, idProveedor) VALUES (\"3\", \"3\");");
-		db.insertData("PiezasDB", "INSERT INTO Suministra (CodigoPieza, idProveedor) VALUES (\"4\", \"4\");");
-		db.insertData("PiezasDB", "INSERT INTO Suministra (CodigoPieza, idProveedor) VALUES (\"5\", \"5\");");
-		System.out.println("Piezas");
-		db.getValues("PiezasDB", "Piezas", "Codigo", "Nombre");
-		System.out.println("Proveedores");
-		db.getValues("PiezasDB", "Proveedores", "ID", "Nombre");
-		System.out.println("Suministra");
-		db.getValues("PiezasDB", "Suministra", "CodigoPieza", "idProveedor", "Precio");
+		db.createDB("CientificosDB");
+		db.createTable("CientificosDB", "CREATE TABLE Cientificos (DNI VARCHAR(8) NOT NULL, NomApels VARCHAR(255), PRIMARY KEY (DNI));");
+		db.createTable("CientificosDB", "CREATE TABLE Proyectos (IDProyecto CHAR(4) NOT NULL, Nombre VARCHAR(100), Horas INT, PRIMARY KEY (IDProyecto));");
+		db.createTable("CientificosDB", "CREATE TABLE Asignado_A (Cientifico VARCHAR(8) NOT NULL, Proyecto CHAR(4) NOT NULL, PRIMARY KEY(Cientifico, Proyecto), FOREIGN KEY (Cientifico) REFERENCES Cientificos(DNI), FOREIGN KEY (Proyecto) REFERENCES Proyectos(IDProyecto));");
+		db.insertData("CientificosDB", "INSERT INTO Cientificos (DNI, NomApels) VALUES (\"1\", \"1\");");
+		db.insertData("CientificosDB", "INSERT INTO Cientificos (DNI, NomApels) VALUES (\"2\", \"2\");");
+		db.insertData("CientificosDB", "INSERT INTO Cientificos (DNI, NomApels) VALUES (\"3\", \"3\");");
+		db.insertData("CientificosDB", "INSERT INTO Cientificos (DNI, NomApels) VALUES (\"4\", \"4\");");
+		db.insertData("CientificosDB", "INSERT INTO Cientificos (DNI, NomApels) VALUES (\"5\", \"5\");");
+		db.insertData("CientificosDB", "INSERT INTO Proyectos (IDProyecto, Nombre) VALUES (\"1\", \"1\");");
+		db.insertData("CientificosDB", "INSERT INTO Proyectos (IDProyecto, Nombre) VALUES (\"2\", \"2\");");
+		db.insertData("CientificosDB", "INSERT INTO Proyectos (IDProyecto, Nombre) VALUES (\"3\", \"3\");");
+		db.insertData("CientificosDB", "INSERT INTO Proyectos (IDProyecto, Nombre) VALUES (\"4\", \"4\");");
+		db.insertData("CientificosDB", "INSERT INTO Proyectos (IDProyecto, Nombre) VALUES (\"5\", \"5\");");
+		db.insertData("CientificosDB", "INSERT INTO Asignado_A (Cientifico, Proyecto) VALUES (\"1\", \"1\");");
+		db.insertData("CientificosDB", "INSERT INTO Asignado_A (Cientifico, Proyecto) VALUES (\"2\", \"2\");");
+		db.insertData("CientificosDB", "INSERT INTO Asignado_A (Cientifico, Proyecto) VALUES (\"3\", \"3\");");
+		db.insertData("CientificosDB", "INSERT INTO Asignado_A (Cientifico, Proyecto) VALUES (\"4\", \"4\");");
+		db.insertData("CientificosDB", "INSERT INTO Asignado_A (Cientifico, Proyecto) VALUES (\"5\", \"5\");");
+		System.out.println("Cientificos");
+		db.getValues("CientificosDB", "Cientificos", "DNI", "NomApels");
+		System.out.println("Proyectos");
+		db.getValues("CientificosDB", "Proyectos", "IDProyecto", "Nombre");
+		System.out.println("Asignado_A");
+		db.getValues("CientificosDB", "Asignado_A", "Cientifico", "Proyecto");
 		db.closeConnection();
 	}
-
-
 }
+
