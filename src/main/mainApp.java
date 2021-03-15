6dd0fb8fa186349d7c6c88874684fe3d794ea797
CRivas <christianrivasp@gmail.com>
2021-03-15 19:58:39 +0100

    6

--- src/main/mainApp.java
+++ src/main/mainApp.java
@@ -25,23 +25,31 @@ public class mainApp {
 	
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		db.connectDB("root", "");
-		db.createDB("DirectoresDB");
-		db.createTable("DirectoresDB", "CREATE TABLE Despachos (Numero INT NOT NULL, Capacidad INT, PRIMARY KEY (Numero));");
-		db.createTable("DirectoresDB", "CREATE TABLE Directores (DNI varchar(8) NOT NULL, NomApels VARCHAR(255), Despacho INT, DNIJefe varchar(8),  PRIMARY KEY(DNI), FOREIGN KEY (Despacho) REFERENCES Despachos(Numero), FOREIGN KEY (DNIJefe) REFERENCES Directores(dni));");
-		db.insertData("DirectoresDB", "INSERT INTO Despachos (Numero, Capacidad) VALUES (\"1\", \"1\");");
-		db.insertData("DirectoresDB", "INSERT INTO Despachos (Numero, Capacidad) VALUES (\"2\", \"2\");");
-		db.insertData("DirectoresDB", "INSERT INTO Despachos (Numero, Capacidad) VALUES (\"3\", \"3\");");
-		db.insertData("DirectoresDB", "INSERT INTO Despachos (Numero, Capacidad) VALUES (\"4\", \"4\");");
-		db.insertData("DirectoresDB", "INSERT INTO Despachos (Numero, Capacidad) VALUES (\"5\", \"5\");");
-		db.insertData("DirectoresDB", "INSERT INTO Directores (DNI, NomApels, Despacho) VALUES (\"1111111A\", \"1\", \"1\");");
-		db.insertData("DirectoresDB", "INSERT INTO Directores (DNI, NomApels, Despacho, DNIJefe) VALUES (\"2222222B\", \"2\", \"2\", \"1111111A\");");
-		db.insertData("DirectoresDB", "INSERT INTO Directores (DNI, NomApels, Despacho, DNIJefe) VALUES (\"3333333C\", \"3\", \"3\", \"1111111A\");");
-		db.insertData("DirectoresDB", "INSERT INTO Directores (DNI, NomApels, Despacho, DNIJefe) VALUES (\"4444444D\", \"4\", \"4\", \"1111111A\");");
-		db.insertData("DirectoresDB", "INSERT INTO Directores (DNI, NomApels, Despacho, DNIJefe) VALUES (\"5555555E\", \"5\", \"5\", \"1111111A\");");
-		System.out.println("Despachos");
-		db.getValues("DirectoresDB", "Despachos", "Numero", "Capacidad");
-		System.out.println("Directores");
-		db.getValues("DirectoresDB", "Directores", "DNI", "NomApels", "Despacho", "DNIJefe");
+		db.createDB("PiezasDB");
+		db.createTable("PiezasDB", "CREATE TABLE Piezas (Codigo INT NOT NULL, Nombre VARCHAR(100), PRIMARY KEY (Codigo));");
+		db.createTable("PiezasDB", "CREATE TABLE Proveedores (ID CHAR(4) NOT NULL, Nombre VARCHAR(100), PRIMARY KEY (ID));");
+		db.createTable("PiezasDB", "CREATE TABLE Suministra (CodigoPieza INT NOT NULL, idProveedor CHAR(4) NOT NULL, Precio INT,  PRIMARY KEY(CodigoPieza, idProveedor), FOREIGN KEY (CodigoPieza) REFERENCES Piezas(Codigo), FOREIGN KEY (idProveedor) REFERENCES Proveedores(id));");
+		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"1\", \"1\");");
+		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"2\", \"2\");");
+		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"3\", \"3\");");
+		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"4\", \"4\");");
+		db.insertData("PiezasDB", "INSERT INTO Piezas (Codigo, Nombre) VALUES (\"5\", \"5\");");
+		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"1\", \"1\");");
+		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"2\", \"2\");");
+		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"3\", \"3\");");
+		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"4\", \"4\");");
+		db.insertData("PiezasDB", "INSERT INTO Proveedores (ID, Nombre) VALUES (\"5\", \"5\");");
+		db.insertData("PiezasDB", "INSERT INTO Suministra (ID, Nombre) VALUES (\"1\", \"1\");");
+		db.insertData("PiezasDB", "INSERT INTO Suministra (ID, Nombre) VALUES (\"2\", \"2\");");
+		db.insertData("PiezasDB", "INSERT INTO Suministra (ID, Nombre) VALUES (\"3\", \"3\");");
+		db.insertData("PiezasDB", "INSERT INTO Suministra (ID, Nombre) VALUES (\"4\", \"4\");");
+		db.insertData("PiezasDB", "INSERT INTO Suministra (ID, Nombre) VALUES (\"5\", \"5\");");
+		System.out.println("Piezas");
+		db.getValues("PiezasDB", "Piezas", "Codigo", "Nombre");
+		System.out.println("Proveedores");
+		db.getValues("PiezasDB", "Proveedores", "ID", "Nombre");
+		System.out.println("Suministra");
+		db.getValues("PiezasDB", "Suministra", "DNI", "CodigoPieza", "idProveedor", "Precio");
 		db.closeConnection();
 	}
 
